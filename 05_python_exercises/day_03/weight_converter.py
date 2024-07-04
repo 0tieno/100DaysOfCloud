67e1ebd1f3986e1f34aa275f74684ee14e877102
Otieno <ronvi035@gmail.com>
2024-07-05 00:01:16 +0300

    day3

---
+++ 05_python_exercises/day_03/weight_converter.py
@@ -0,0 +1,10 @@
+weight = int(input("weight "))
+unit = input('(L)bs or (K)g')
+
+if unit.upper() == 'L':
+    converted = weight * 0.45
+    print(f"you are {converted} kilos")
+else:
+    converted = weight /0.45
+    print(f"you are {converted}bs")
+    
