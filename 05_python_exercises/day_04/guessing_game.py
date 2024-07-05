07c01f4609d054ac6bf0a402badcdee88667830d
Otieno <ronvi035@gmail.com>
2024-07-05 09:06:45 +0300

    added guess game

---
+++ 05_python_exercises/day_04/guessing_game.py
@@ -0,0 +1,17 @@
+# Guessing game
+
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+
+
+while guess_count < guess_limit:
+    guess = int(input('GUESS: '))
+    guess_count += 1
+    if guess  == secret_number:
+        print("You won!")
+        break
+
+
+else:
+    print("Sorry you failed!")
---
+++ 05_python_exercises/day_04/while_loop.py
@@ -0,0 +1,15 @@
+# i = 1
+
+# while i <= 5:
+#     print(i)
+    
+# print("Done")
+
+
+# i = 1
+
+# while i<=5:
+#     print("*" * i)
+#     i+=1
+
+
